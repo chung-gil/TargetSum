db771fd3e2d0f1b36dd094102dc98fc4bb549791
chunggil han <changgilhan@gmail.com>
2021-07-17 20:25:43 -0400

    first commit

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+// TouchableOpacity
+// TouchableHighlight
+
+class RandomNumber extends React.Component {
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        number: PropTypes.number.isRequired,
+        isDisabled: PropTypes.bool.isRequired,
+        onPress: PropTypes.func.isRequired,
+    };
+
+    handlePress = () => {
+        if(this.props.isDisabled) { return; };
+        this.props.onPress(this.props.id);
+    };
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this.handlePress}>
+                <Text style={[styles.random, this.props.isDisabled && styles.selected]}>{this.props.number}</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    random: {
+        backgroundColor: '#999',
+        width: 100,
+        marginHorizontal: 15,
+        marginVertical: 25,
+        fontSize: 35,
+        textAlign: 'center',        
+    },
+
+    selected: {
+        opacity: 0.3,
+    }
+
+});
+
+export default RandomNumber;
